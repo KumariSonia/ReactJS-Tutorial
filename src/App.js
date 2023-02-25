680b38b1becfa872517f4b09615146cb300f894b
milan bamaniya <milan@fliptree.com>
2023-02-25 11:22:37 +0530

    5th video completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <nav className="nav">
+    <a className="nav-link active" aria-current="page" href="/">TextUtils</a>
+    <a className="nav-link" href="/">Home</a>
+    <a className="nav-link" href="/">About</a>
+  </nav> 
+  );
+}
+
+export default App;
